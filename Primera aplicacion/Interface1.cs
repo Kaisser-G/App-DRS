6844c3127fd77eae2b6aa783df2c77a0cc2db2ec
Kaisser-G <agustingalarza001@gmail.com>
2018-09-30 19:23:01 -0300

    Revert "Revert "Proyecto DRS""
    This reverts commit 76c77dbe586b0e71a4e57cb9af63678d31618294.

---
+++ Primera aplicacion/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primera_aplicacion
+{
+    public interface ISerie
+    {
+       void reasignarData(double dataLat, double dataLng);
+    }
+}
